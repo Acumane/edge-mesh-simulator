83f5b5791154418e5653fec3040974cbf3ac7712
Bren <git@bren.page>
2024-07-18 18:55:17 -0700

    edge gradient + alpha, treshold <[, ]>

--- vis/src/edges.ts
+++ vis/src/edges.ts
@@ -1,21 +1,32 @@
 import * as THREE from "three"
 import { controllers, Controller } from "./controllers"
+import { OFFSET } from "./factory"
 import { scene } from "./setup"
 
 const MAX_VERT = 10000
 let lineMesh: THREE.LineSegments
+let edgeThreshold = 66
+
+const strenCol: { [key: number]: THREE.Color } = {
+    100: new THREE.Color(0x00ff00),
+    90:  new THREE.Color(0x00ff00),
+    60:  new THREE.Color(0xffff00),
+    30:  new THREE.Color(0xff8000),
+    0:   new THREE.Color(0xff0000)
+}
 
 export function initEdges() {
     const geometry = new THREE.BufferGeometry(),
         positions = new Float32Array(MAX_VERT * 6), // 3<x,y,z> * 2
-        colors = new Float32Array(MAX_VERT * 6) // 3<r,g,b> * 2
+        colors = new Float32Array(MAX_VERT * 8)     // 4<r,g,b,a> * 2
 
     geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3))
-    geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3))
+    geometry.setAttribute("color", new THREE.BufferAttribute(colors, 4))
 
-    const material = new THREE.LineBasicMaterial({ vertexColors: true })
+    const material = new THREE.LineBasicMaterial({ vertexColors: true, transparent: true })
     lineMesh = new THREE.LineSegments(geometry, material)
-    lineMesh.position.set(-20, 0, 10)
+    lineMesh.position.set(...OFFSET.toArray())
+    lineMesh.frustumCulled = false
     scene.add(lineMesh)
 }
 
@@ -27,7 +38,7 @@ export function updateEdges() {
     for (let a = 0; a < cVals.length && lines < MAX_VERT; a++) {
         for (let b = a + 1; b < cVals.length && lines < MAX_VERT; b++) {
             const stren = Math.min(cVals[a].hears[cVals[b].name], cVals[b].hears[cVals[a].name])
-            if (stren > 0) {
+            if (stren > edgeThreshold) {
                 updateLine(lines, cVals[a], cVals[b], stren)
                 lines++
             }
@@ -36,15 +47,28 @@ export function updateEdges() {
     position.needsUpdate = color.needsUpdate = true
 }
 
-function updateLine(index: number, cA: Controller, cB: Controller, stren: number) {
+function updateLine(i: number, cA: Controller, cB: Controller, stren: number) {
     const { position, color } = lineMesh.geometry.attributes
-    position.array.set([cA.pos.x, cA.pos.z, -cA.pos.y, cB.pos.x, cB.pos.z, -cB.pos.y], index * 6)
-    const { r, g, b } = getLineColor(stren)
-    color.array.set([r, g, b, r, g, b], index * 6)
+    position.array.set([cA.pos.x, cA.pos.z, -cA.pos.y, cB.pos.x, cB.pos.z, -cB.pos.y], i * 6)
+    const { r, g, b, a } = getLineColor(stren)
+    color.array.set([r, g, b, a, r, g, b, a], i * 8)
 }
 
-function getLineColor(stren: number): THREE.Color {
-    if (stren < 33) return new THREE.Color(1, 0, 0)
-    if (stren < 66) return new THREE.Color(1, 1, 0)
-    return new THREE.Color(0, 1, 0)
+function getLineColor(stren: number) {
+    const levels = Object.keys(strenCol).map(Number),
+        i = levels.findIndex((level) => stren <= level),
+        lower = levels[Math.max(0, i - 1)],
+        upper = levels[i]
+
+    return {
+        ...strenCol[lower].clone().lerp(strenCol[upper], (stren - lower) / (upper - lower)),
+        a: stren > 50 ? 1.0 : 0.4 + (stren / 50) * 0.6
+    }
+}
+
+export function threshold(step: number) {
+    return (event: any) => {
+        edgeThreshold = Math.min(Math.max(edgeThreshold + step, 0), 100)
+        updateEdges()
+    }
 }
