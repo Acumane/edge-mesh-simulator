7eeec742d373b415149d552b67cc38a0ad281d98
Bren <git@bren.page>
2024-08-16 15:02:51 -0700

    use .GLB, style & cleanup

--- sim/signal/run.py
+++ sim/signal/run.py
@@ -5,7 +5,7 @@ from attrs import define, Factory as new
 from tracer import Tracer
 from vis import visualize
 
-SCENE_FILE = "poznan.obj"
+SCENE = "scene.glb"
 
 @define
 class Timer:
@@ -14,24 +14,28 @@ class Timer:
         return f"{time() - self._start:.4f}s"
 
 def main():
-    mesh = trimesh.load_mesh(SCENE_FILE)
+    scene, meshes = trimesh.load(SCENE), []
+    for _, geometry in scene.geometry.items(): # type: ignore
+        if isinstance(geometry, trimesh.Trimesh): meshes.append(geometry)
+    mesh = trimesh.util.concatenate(meshes) if len(meshes) > 1 else meshes[0]  # single mesh
+
     vertices, faces = mesh.vertices, mesh.faces # type: ignore
 
     tracer = Tracer()
-    tracer.load_scene(vertices, faces)
+    tracer.load(vertices, faces)
 
-    tx_pos, rx_pos = [0, 15, 0], [-30, 1.5, 45]
+    a, b = [100, 60, 5], [0, 10, 20]
 
     timer = Timer()
-    result = tracer.trace(tx_pos, rx_pos)
+    result = tracer.trace(a, b)
     print(f"[{timer}] Tracing complete")
     print("Trace result:", result)
 
-    tx_freq = 2.4e9  # 2.4 GHz
+    freq = 2.4e9  # 2.4 GHz
     permittivity = 5.3  # Concrete
-    losses = tracer.get_total_loss(tx_pos, rx_pos, result, tx_freq, permittivity)
+    losses = tracer.getLoss(a, b, result, freq, permittivity)
     print(f"[{timer}] Found losses: {losses}")
 
-    visualize(mesh, tx_pos, rx_pos, result) # type: ignore
+    visualize(mesh, a, b, result) # type: ignore
 
 main()
